5b20883a31a5750fc367df82986819ac6319b630
Miyawaki <tetsu.miyawaki@gmail.com>
2022-01-20 13:52:16 +0000

    [Finish]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
