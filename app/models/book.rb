e7a62f9e4beae6ef16eeb21c3d5b6f78735b6dac
Miyawaki <tetsu.miyawaki@gmail.com>
2022-01-21 07:50:11 +0000

    [Update]4-25

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
    has_one_attached :image
    belongs_to :user
 
-   validates :body, presence: true
-   validates :title, presence: true, length: {maximum: 200}
+   validates :body, presence: true, length: {maximum: 200}
+   validates :title, presence: true
 end
